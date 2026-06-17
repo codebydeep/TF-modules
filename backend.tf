e2d78012cb495593157153d922571c1461f69e48
codebydeep <deepanshvish123@gmail.com>
2026-06-17 19:56:24 +0530

    resolve- tf-format-issues

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
-    backend "s3" {
-        bucket = "terraform-s3-bucket-8134"
-        key = "terraform.tfstate"
-        region = "eu-north-1"
-        use_lockfile = true
-    }
+  backend "s3" {
+    bucket       = "terraform-s3-bucket-8134"
+    key          = "terraform.tfstate"
+    region       = "eu-north-1"
+    use_lockfile = true
+  }
 }
